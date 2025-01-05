d360b253a74a361cec618dca3862193e88c0aaf5
carunaaa <devkotakaruna06@gmail.com>
2025-01-05 14:28:39 +0545

    minor changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: FC<NavbarProps> = ({ toggleTheme, isDarkMode }) => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-fall-100 dark:bg-fall-900 shadow-lg">
       <div className="flex items-center justify-between relative">
-        <div className="text-4xl text-fall-900 dark:text-fall-50 tracking-wider">
+        <div className="text-4xl text-gray-800 dark:text-fall-50 tracking-wider font-extrabold">
           <h1>k.</h1>
         </div>
 
